fb120b063d9ed6b6eef5b6bdde0609caf738a9b2
Dynamicwang <1084745541@qq.com>
2019-08-16 16:16:27 +0800

    Create adc.h

---
+++ adc.h
@@ -0,0 +1,12 @@
+#ifndef ADC_H_
+#define ADC_H_
+
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_dma.h"
+#include "stm32f10x_adc.h"
+
+void ADC1_Config(void);
+
+#endif
