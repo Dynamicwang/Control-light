e8e0de18e635a6e77b1bb9440653986771ef5ff4
Dynamicwang <1084745541@qq.com>
2019-08-16 16:17:47 +0800

    Create TIM.h

---
+++ TIM.h
@@ -0,0 +1,15 @@
+#ifndef TIM_H_
+#define TIM_H_
+
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_tim.h"
+
+
+
+void TIM3_Config(u16 x);
+
+
+
+#endif
