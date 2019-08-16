730013b1e924f1a7016d4aa7b2c54c9c75c2ec09
Dynamicwang <1084745541@qq.com>
2019-08-16 16:20:20 +0800

    Create usart.h

---
+++ usart.h
@@ -0,0 +1,14 @@
+#ifndef USART_H_
+#define USART_H_
+
+
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stdio.h"
+
+void USART1_Config(void);
+
+#endif
+
